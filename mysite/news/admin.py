dd801436e6b43ce9547afeb929be7ec9c70930b7
Yurii Serediuk <yurii.seredyuk@gmail.com>
2022-09-13 21:23:15 +0300

    Django: media

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    list_filter = ('author', 'created_date', 'published_date')
+    search_fields = ('title', 'text')
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published_date'
+    ordering = ['author', 'published_date']
+    fields = ( 'image_tag', )
+    # readonly_fields = ('image_tag',)
+
+admin.site.register(Post, PostAdmin)
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -8,10 +8,17 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    # def image_tag(self):
+    #     from django.utils.html import escape
+    #     return u'<img src="%s" />' % escape('users/%Y/%m/%d/')
+    # image_tag.short_description = 'Image'
+    # image_tag.allow_tags = True
 
     def __str__(self):
        return f"{self.author} {self.title}" 
